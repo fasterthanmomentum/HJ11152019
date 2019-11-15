c28c70a0a73a11710bb4760f50990f1a3296f912
Joy Best <fasterthanmomentum@gmail.com>
2019-11-15 17:10:33 -0500

    Initial Commit

---
+++ HJ11152019Tests/HJ11152019Tests.swift
@@ -0,0 +1,34 @@
+//
+//  HJ11152019Tests.swift
+//  HJ11152019Tests
+//
+//  Created by JOY BEST on 11/15/19.
+//  Copyright © 2019 JOY BEST. All rights reserved.
+//
+
+import XCTest
+@testable import HJ11152019
+
+class HJ11152019Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
